b140fc5c94fc4cadb629109820403c95a927ae4b
ArielAVega <ariel.vega@educacionit.com>
2023-10-10 09:52:02 -0300

    Update project: create scene and player (include: script and movement)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello ");
+        transform.position = Vector3.zero;
+        speed = 3.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Ariel Alejandro Vega");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+    }
+}
